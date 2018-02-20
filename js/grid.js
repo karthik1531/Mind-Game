4cd4d85a19824882b5e23fbc716943757b9779f5
Ankush Dharkar <ankushdharkar@gmail.com>
2018-02-20 23:33:16 +0530

    Refactored code to make it better and functional

--- js/grid.js
+++ js/grid.js
@@ -1,25 +1,50 @@
+function setUpGrid(Rcount, Ccount) {
+
+	var mainGridContainer = $(".main-grid-container");
+
+	for (var i = 0; i < Rcount; i++) { // Row
+
+		var newRow = $("<div class=\"single-row row-"+i+"\"></div>");
+
+		for (var j = 0; j < Ccount; j++) { // col
+			var newCol = $("<div class=\"single-col col-"+j+"\"></div>");
+		
+
+			newRow.append(newCol);
+
+		}
+
+		mainGridContainer.append(newRow);
+	}
+}
+
+
+function attachColFunctions() {
+
+	$('.single-col').on('click', function(){
+		$(this).css('background-color', 'red');
+	});
+
+}
+
+
 $(document).ready(function(){
+	
 	var colors = ['blue','black','red','green','yellow','orange','pink','grey','voilet','white','purple'];
 
-	$("button").on('click', function(){
+
+
+	$(".btn-submit").on('click', function(){
 		const Rcount=$('#row').val();
 		const Ccount=$('#columnn').val();
-		var $container = $("<div></div>").css("float","left");
+		
 		if((Rcount == 0 && Ccount==0)){                           //To Manage Zero value input
 			alert("Please enter a valid input");
 		}
 		else{
-			for (var i = 0; i < Rcount; i++) {
-				for (var j = 0; j < Ccount; j++) {
-					$("<div></div>").addClass("grid").appendTo($container);
-				}
-				$("<div></div>").css("clear", "both").appendTo($container);
-			}
-			$container.appendTo($("p"));
+			setUpGrid(Rcount, Ccount);
+			attachColFunctions();
 		}
 	});
-	$('.box').on('click', function(){
-		alert('hi');
-		$(this).css('background-color', 'black');
-	});
+
 });
