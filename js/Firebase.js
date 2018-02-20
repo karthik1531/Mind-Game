61477e94c533a75b1c004080ce97ecb96303e24e
Karthik <karthikreddylingareddy@gmail.com>
2018-02-20 22:17:29 +0530

    Added a program for firebase and changed the grid program to change background colors with some errors

---
+++ js/Firebase.js
@@ -0,0 +1,17 @@
+var databaseRef=firebase.database().ref('users/');
+var count=0;
+$('#add').on('click', function(){
+	alert('hi');
+	count=count+1;
+  alert(count);
+  var data = {
+  	count : count
+  }
+  var updates = {};
+  updates['/users/']= data;
+  firebase.database().ref.update(updates);
+  });
+$('#sub').click(function(){
+	count=count-1;
+  alert(count);
+});
