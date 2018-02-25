83fdd9e6c38ad7a1a10a8f5e6ca8408c2bcf865e
Karthik <karthikreddylingareddy@gmail.com>
2018-02-25 22:59:49 +0530

    Changed the grid program to assign colors dynamically from array of 10

--- js/firebase-app.js
+++ js/firebase-app.js
@@ -1,24 +1,34 @@
 // var databaseRef=firebase.database().ref('users/');
-// var userRef = firebase.database().ref('users/');
+var userRef = firebase.database().ref('users');
+var count = 0;
+var count;
+userRef.on('value', gotData);
+function gotData(data){
+	var  key = Object.keys('users');
+	console.log(key);
+	console.log(data);
+	//console.log(data.count);
+	//var count=data.count;
+}
 
-var count=0;
-
-$('#add').on('click', function(){
+$('#add-btn').on('click', function(){
 
 	count=count+1;
 	alert(count);
-
+	var uid= userRef.push().key;
 	var data = {
+		userId: uid,
 		count: count
 	};
 
 	var updates = {};
+	//console.log(uid);
 
-	updates['/users/'] = data;
+	updates['/users/' + uid] = data;
 
-	firebase.database().ref().update(updates);
+	//firebase.database().ref().update(updates);
 
-	// userRef.update(data);
+	userRef.update(data);
 
 });
 
--- js/grid.js
+++ js/grid.js
@@ -1,15 +1,17 @@
 function setUpGrid(Rcount, Ccount) {
 
 	var mainGridContainer = $(".main-grid-container");
+	
 
 	for (var i = 0; i < Rcount; i++) { // Row
 
 		var newRow = $("<div class=\"single-row row-"+i+"\"></div>");
 
 		for (var j = 0; j < Ccount; j++) { // col
-			var newCol = $("<div class=\"single-col col-"+j+"\"></div>");
+			
+			var newCol = $("<div class=\"single-col "+j+"\"></div>");
 		
-
+			newCol.attr('id',j);
 			newRow.append(newCol);
 
 		}
@@ -19,32 +21,43 @@ function setUpGrid(Rcount, Ccount) {
 }
 
 
-function attachColFunctions() {
+function attachColFunctions(e) {
 
-	$('.single-col').on('click', function(){
-		$(this).css('background-color', 'red');
+	var colors = ['blue','black','red','green','yellow','orange','pink','grey','voilet','white','purple'];
+	var click = 0;
+	$('.single-col').on('click', function(e){
+		var id = $(e.target).attr('id');
+
+		if((click%2)==0){
+			$(e.target).css('background-color', colors[(id%10)]);
+			click = click+1;
+		}
+		else{
+			$(e.target).css('background-color', 'yellow');
+			click = click+1;
+		}
 	});
-
 }
 
 
 $(document).ready(function(){
 	
-	var colors = ['blue','black','red','green','yellow','orange','pink','grey','voilet','white','purple'];
-
-
-
 	$(".btn-submit").on('click', function(){
 		const Rcount=$('#row').val();
 		const Ccount=$('#columnn').val();
-		
-		if((Rcount == 0 && Ccount==0)){                           //To Manage Zero value input
+		var gridSize = Rcount * 50;
+		$('.main-grid-container').css('width', gridSize);
+		if((Rcount <= 0 && (Ccount <= 0)) && ($.isNumeric(Rcount)) && ($.isNumeric(Ccount))){                           //To Manage Zero value input
 			alert("Please enter a valid input");
 		}
 		else{
 			setUpGrid(Rcount, Ccount);
 			attachColFunctions();
+			alert("else");
 		}
 	});
 
+	$(".btn-cancel").on('click', function(){
+		location.reload(true);
+	});
 });
